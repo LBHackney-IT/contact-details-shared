526f8d0bdc68cf08012c4d04c5672a3f38813507
humulla <humairaa.mulla@hackney.gov.uk>
2021-07-29 14:44:50 +0100

    shared contact details

---
+++ Hackney.Shared.ContactDetails/SNS/ContactDetailsConstants.cs
@@ -0,0 +1,8 @@
+namespace Hackney.Shared.ContactDetails.Sns
+{
+    public static class ContactDetailsConstants
+    {
+        public const string CREATED = "ContactDetailAddedEvent";
+        public const string DELETED = "ContactDetailDeletedEvent";
+    }
+}
